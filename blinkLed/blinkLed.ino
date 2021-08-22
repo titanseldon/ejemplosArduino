31ab06471f765d32ebb49bf23422ea68455b887f
harsel <tigrejdvz@gmail.com>
2021-08-22 15:50:12 -0500

    Primer commit, led parpadenado

---
+++ blinkLed/blinkLed.ino
@@ -0,0 +1,12 @@
+#define PIN_LED 12
+
+void setup() {
+  pinMode(PIN_LED, OUTPUT);
+}
+
+void loop() {
+  digitalWrite(PIN_LED, HIGH);
+  delay(1000);
+  digitalWrite(PIN_LED, LOW);
+  delay(2000);
+}
